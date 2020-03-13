6f808e690f65592341265115c711bcd7ff2425c9
strrugleforlife <kuro.y6222@gmail.com>
2020-03-13 21:59:09 +0900

    test

---
+++ strrugleforlife-web/src/test/java/jp/co/strrugleforlife/StrrugleforlifeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.strrugleforlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrrugleforlifeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
